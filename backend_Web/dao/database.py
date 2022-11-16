e0979ce3eb83d885e7638e51df5295fa0294a876
Ebernauta <104079428+ebernauta@users.noreply.github.com>
2022-11-16 17:16:47 -0300

    se borrar directorio backend

--- backend_Web/dao/database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-def conexion():
-    try:
-        client = MongoClient("mongodb://localhost:27017")
-        db = client['Usuarios']
-    except ConnectionError:
-        print('Error de conexión con la base de datos')
-    return db
