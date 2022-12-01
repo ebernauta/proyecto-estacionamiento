dc121f27a4b98cb010b658878f57f640552b18c2
Ebernauta <ebernauta18@gmail.com>
2022-11-30 22:05:18 -0300

    Modificacion mongo db para conexion a python everywhere

--- dao/database.py
+++ dao/database.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 def conexion():
     try:
-        client = MongoClient("mongodb+srv://adminProyect:acceso123@sistema-estacionamiento.ieortuz.mongodb.net/?retryWrites=true&w=majority")
-        db = client.test
+        uri = "mongodb+srv://adminProyect:acceso123@sistema-estacionamiento.ieortuz.mongodb.net/?retryWrites=true&w=majority"
+        client = MongoClient(uri)
         print("Conectado a la base de datos")
     except ConnectionError:
         print('Error de conexión con la base de datos')
-    return db
+    return client
+
 
