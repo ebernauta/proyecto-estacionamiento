b9b3719726f6f00f6ea7d4b12d2303247959e7fc
Ebernauta <ebernauta18@gmail.com>
2022-11-30 22:10:04 -0300

    Ultimo intento para subir web

--- dao/database.py
+++ dao/database.py
@@ -1,8 +1,8 @@
-from pymongo import MongoClient
+import pymongo
 def conexion():
     try:
         uri = "mongodb+srv://adminProyect:acceso123@sistema-estacionamiento.ieortuz.mongodb.net/?retryWrites=true&w=majority"
-        client = MongoClient(uri)
+        client = pymongo.MongoClient(uri)
         print("Conectado a la base de datos")
     except ConnectionError:
         print('Error de conexión con la base de datos')
