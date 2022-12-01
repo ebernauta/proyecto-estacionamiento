8311a951469f6927b1b43977d3963542d89786ac
Ebernauta <ebernauta18@gmail.com>
2022-11-30 22:32:25 -0300

    Cambio final para levantar web

--- dao/database.py
+++ dao/database.py
@@ -1,11 +1,9 @@
-import pymongo
+from pymongo import MongoClient
 def conexion():
     try:
-        uri = "mongodb+srv://adminProyect:acceso123@sistema-estacionamiento.ieortuz.mongodb.net/?retryWrites=true&w=majority"
-        client = pymongo.MongoClient(uri)
+        client = MongoClient("mongodb+srv://adminProyect:acceso123@sistema-estacionamiento.ieortuz.mongodb.net/?retryWrites=true&w=majority")
+        db = client.test
         print("Conectado a la base de datos")
     except ConnectionError:
         print('Error de conexión con la base de datos')
-    return client
-
-
+    return db
